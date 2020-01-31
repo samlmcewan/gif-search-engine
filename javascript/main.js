d1208e164826a4db3cb5bf76a0f41bfe60341473
samlmcewan <samlmcewan@gmail.com>
2020-01-31 22:02:42 +0000

    Both pressing enter and click events now display the input
    -moved the event listener to the form
    -changed the button type to submit
    -added e.preventDefault();

--- javascript/main.js
+++ javascript/main.js
@@ -1,32 +1,13 @@
-
 // Get the input value
-
-document.querySelector(".js-go").addEventListener('click',function(){
-
+document.querySelector("#form").addEventListener('submit',function(e){
   var input = document.querySelector("input").value;
   pushToDOM(input);
-
   
+  e.preventDefault();
 });
-
-document.querySelector(".js-userinput").addEventListener('keyup',function(e){
-
-   var input = document.querySelector("input").value;
-
-  // Push to DOM if the key ENTER is pressed
-  if(e.which === 13) {
-    pushToDOM(input);
-  }
-
-});
-
 
 // Display the input
-
 function pushToDOM(input) {
-
 	var container = document.querySelector(".js-container");
-  	container.innerHTML = input;
-
-
+  container.innerHTML = input;
 }
